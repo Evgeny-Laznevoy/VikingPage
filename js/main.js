36ebdae5bcd7105e6a17bf86aacc7416cc473b43
Evgeny Laznevoy <48503485+Evgeny-Laznevoy@users.noreply.github.com>
2020-10-27 16:25:05 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,14 +7,15 @@ $(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.heroes__slider-text',
-        prevArrow: '<button class="slick-btn slick-prev"><img src="/images/arrow-left.png" alt="prev"></button>',
-        nextArrow: '<button class="slick-btn slick-next"><img src="/images/arrow-right.png" alt="next"></button>',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
         responsive: [{
           breakpoint: 769,
           settings: {
             arrows: false,
           }
-        },]
+        }
+       ]
       });
       $('.heroes__slider-text').slick({
         slidesToShow: 1,
