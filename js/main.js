6a264d3a9d3f0c9b8f7bf389b012387fab19335d
Evgeny Laznevoy <48503485+Evgeny-Laznevoy@users.noreply.github.com>
2020-10-27 16:20:58 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,26 @@
-// $(function(){
-//     $('[data-fancybox]').fancybox({
-//         youtube : {
-//             controls : 0,
-//             showinfo : 0
-//         }
-//     });
-// });
+$(function(){
+    $('.menu__btn').on('click', () => {
+      $('.menu__list').toggleClass('menu__list--active')
+    });
 
-$('.menu__btn').on('click', () => {
-  $('.menu__list').toggleClass('menu__list--active')
-});
-
-$('.heroes__slider-img').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.heroes__slider-text',
-    prevArrow: '<button class="slick-btn slick-prev"><img src="/images/arrow-left.png" alt="prev"></button>',
-    nextArrow: '<button class="slick-btn slick-next"><img src="/images/arrow-right.png" alt="next"></button>',
-    responsive: [{
-      breakpoint: 769,
-      settings: {
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="/images/arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="/images/arrow-right.png" alt="next"></button>',
+        responsive: [{
+          breakpoint: 769,
+          settings: {
+            arrows: false,
+          }
+        },]
+      });
+      $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
         arrows: false,
-      }
-    },]
-  });
-  $('.heroes__slider-text').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    arrows: false,
-    asNavFor: '.heroes__slider-img',
+        asNavFor: '.heroes__slider-img',
+      });
   });
